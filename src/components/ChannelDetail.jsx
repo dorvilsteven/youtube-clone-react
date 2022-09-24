207b08b8c25534322328ec80315f10059404e9f5
Steven Dorvil <dorvilsteven@gmail.com>
2022-09-24 00:03:44 -0400

    created nav bar with icon logo and search bar, used react router dom to create the nav and marterial ui to create the look and feel

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ChannelDetail = () => {
+    return (
+        <div>Channel Detail</div>
+    )
+}
+
+export default ChannelDetail;
---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SearchFeed = () => {
+    return (
+        <div>Search Feed</div>
+    )
+}
+
+export default SearchFeed;
